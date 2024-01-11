e0952a21d7dd47ccbce0e4bfae6d9da9628efef6
igornardoni <igornardonik@gmail.com>
2024-01-11 10:56:59 -0300

    serie

--- screen_match_v2/src/Principal.java
+++ screen_match_v2/src/Principal.java
@@ -1,9 +1,10 @@
 import br.com.alura.screenmatch.models.Filme;
+import br.com.alura.screenmatch.models.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setNome("O Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1972);
         meuFilme.setDuracaoEmMinutos(175);
 
@@ -11,10 +12,26 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println("Total de avaliações: \n" + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.pegaMedia());
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.printf("Média das notas: %.1f\n", meuFilme.pegaMedia());
 //        meuFilme.somaDasAvaliacoes = 10;
 //        meuFilme.totalDeAvaliacoes = 1;
+
+        System.out.println();
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2004);
+        lost.setNumeroDeTemporadas(6);
+        lost.setEpisodiosPorTemporada(22);
+        lost.setMinutosPorEpisodio(43);
+        lost.setAtiva(false);
+        lost.exibeFichaTecnica();
+        System.out.printf("Temporadas: %d temporadas \n", lost.getNumeroDeTemporadas());
+        System.out.printf("Episódios por temporada: %d episódios \n", lost.getEpisodiosPorTemporada());
+        System.out.printf("Minutos por episódio: %d minutos \n", lost.getMinutosPorEpisodio());
+        System.out.println("Lost ainda está em exibição? " + lost.isAtiva());
+
 
     }
 }
---
+++ screen_match_v2/src/br/com/alura/screenmatch/models/Serie.java
@@ -0,0 +1,40 @@
+package br.com.alura.screenmatch.models;
+
+public class Serie extends Titulo{
+    private int numeroDeTemporadas;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getNumeroDeTemporadas() {
+        return numeroDeTemporadas;
+    }
+
+    public void setNumeroDeTemporadas(int numeroDeTemporadas) {
+        this.numeroDeTemporadas = numeroDeTemporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
